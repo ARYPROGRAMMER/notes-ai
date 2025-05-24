dba81a8efd880de726557f3e159dc800ee8ca6d2
Arya Pratap Singh <notaryasingh@gmail.com>
2025-05-24 21:31:33 +0530

    feat: adding sidebar details
    Signed-off-by: Arya Pratap Singh <notaryasingh@gmail.com>

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,6 @@
 import { RootLayout, Sidebar, Content, DraggableTopBar } from '@/components'
 import { ActionButtonsRow } from '@/components'
+import NotePreviewList from './components/NotePreviewList'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonsRow className="flex justify-between mt-1" />
+          <NotePreviewList className="mt-3 space-y-1"/>
         </Sidebar>
         <Content className="border-l bg-zinc-900/50 border-l-white/20">Content</Content>
       </RootLayout>
